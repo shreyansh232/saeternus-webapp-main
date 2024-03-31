f50b138d637865b6b3ebc5eda00fd9bcf59981b1
Javed Habib <jh4official@gmail.com>
2024-04-01 02:45:24 +0530

    feat: add hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,54 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { ArrowRight } from 'lucide-react';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<>
 			<MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
-				Hello world!
+				<h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+					Innovating for your
+					<span className='text-primary'> success</span>
+				</h1>
+				<p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+					<span className='text-primary'>Your success is our success</span>. We
+					are committed to delivering innovative solutions and exceptional
+					service to help you achieve your goals.{' '}
+					<span className='text-primary'>
+						Let us help you turn your vision into a reality
+					</span>
+					.
+				</p>
+
+				<Link
+					className={buttonVariants({
+						size: 'lg',
+						className: 'mt-5',
+					})}
+					href='/courses'
+					target='_blank'
+				>
+					Get started
+					<ArrowRight className='ml-2 h-5 w-5' />
+				</Link>
 			</MaxWidthWrapper>
+			<div>
+				<div className='relative isolate'>
+					<div
+						aria-hidden='true'
+						className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+					>
+						<div
+							style={{
+								clipPath:
+									'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+							}}
+							className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+						/>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
