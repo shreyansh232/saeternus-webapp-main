6a198490ac31389c17cb7bf413d72406ee3d99c6
Violetcv <you@example.com>
2024-05-27 19:35:57 +0530

    Feat: add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { satoshi } from '@/utils/customfonts';
-
-// const inter = Inter({ subsets: ['latin'] });
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: 'Saeternus',
@@ -20,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <GoogleTagManager gtmId='GTM-W4HCJHQQ' />
       <body
         className={cn(
           'min-h-screen antialiased',
