c07ca5525bbf1dccb39233cbac24fda9c1b3a731
Violetcv <you@example.com>
2024-06-02 12:07:19 +0530

    responsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,10 +12,9 @@ export default function About() {
           layout='fill'
           objectFit='cover'
           quality={100}
-          // priority
           className='absolute inset-0 h-full w-full object-cover'
         />
-        <div className='absolute inset-0 bg-black bg-opacity-70 p-40 text-white'>
+        <div className='absolute inset-0 bg-black bg-opacity-70 p-10 text-white md:p-40'>
           <h1 className='text-sm font-thin'>ABOUT US</h1>
           <p className='relative text-4xl'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
@@ -23,10 +22,12 @@ export default function About() {
           </p>
         </div>
       </div>
-      <div className='bg-white p-40'>
-        <div className='flex items-center'>
-          <h1 className='absolute mr-20 text-2xl font-bold'>Who we are</h1>
-          <p className='text-bold ml-40'>
+      <div className='bg-white p-10 md:p-40'>
+        <div className='flex flex-col items-center md:flex-row'>
+          <h1 className='text-2xl font-bold md:absolute md:mr-20 md:text-4xl'>
+            Who we are
+          </h1>
+          <p className='text-bold md:ml-52'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
             facere minima, vel, voluptatibus, nobis tempora corporis officiis
             esse mollitia nostrum quia minus necessitatibus voluptatem.
