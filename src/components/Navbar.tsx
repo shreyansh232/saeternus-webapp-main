0d5c83b918c9679ea50d10a32435379502aef7c5
Shreyansh Pandey <shreyansh487@gmail.com>
2024-06-15 21:04:02 +0530

    link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export interface Navigation {
 
 const navigations: Navigation[] = [
   { path: '/courses', name: 'Courses', authNeeded: true },
-  { path: '/knowledge', name: 'Knowledge Haven', authNeeded: true },
+  // { path: '/knowledge', name: 'Knowledge Haven', authNeeded: true },
   { path: '/about', name: 'About', authNeeded: true },
   { path: '/contact', name: 'Contact', authNeeded: true },
 ];
