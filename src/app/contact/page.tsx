9504f5a5d6fa674d6cabc7965d81d6f4ffca18b8
Shreyansh Pandey <shreyansh487@gmail.com>
2024-07-16 22:41:40 +0530

    fix: linting

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useForm } from 'react-hook-form';
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC } from 'react';
 import { sendEmail } from '@/utils/email';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -14,54 +14,20 @@ import {
   faPhone,
 } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { motion } from 'framer-motion';
 
 export type FormData = {
   name: string;
   email: string;
   number: string;
-  subject: string;
   message: string;
 };
 
 const ContactPage: FC = () => {
   const { register, handleSubmit } = useForm<FormData>();
-  const [contactFormInView, setContactFormInView] = useState(false);
-  const contactFormRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const observerOptions = {
-      threshold: 0.5,
-    };
-
-    const contactFormObserver = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setContactFormInView(true);
-        } else {
-          setContactFormInView(false);
-        }
-      });
-    }, observerOptions);
-
-    if (contactFormRef.current) {
-      contactFormObserver.observe(contactFormRef.current);
-    }
-
-    return () => {
-      if (contactFormRef.current) {
-        contactFormObserver.unobserve(contactFormRef.current);
-      }
-    };
-  }, []);
 
   function onSubmit(data: FormData) {
     sendEmail(data);
   }
-  const animationVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0 },
-  };
 
   return (
     <div>
@@ -71,10 +37,10 @@ const ContactPage: FC = () => {
             <div className='mb-20'>
               <h1 className='text-7xl font-extrabold'>Let&apos;s Talk</h1>
               <p className='text-md mt-10 font-normal text-gray-600'>
-                We are committed to delivering{' '}
+                Your success is our success.{' '}
                 <span className='text-primary'>
-                  innovative solutions and exceptional service to help you
-                  achieve your goals.
+                  We are committed to delivering innovative solutions and
+                  exceptional service to help you achieve your goals.
                 </span>{' '}
                 Let us help you turn your vision into a reality.{' '}
               </p>
@@ -143,122 +109,99 @@ const ContactPage: FC = () => {
             </div>
           </div>
           {/* contact from */}
-          <motion.div
-            ref={contactFormRef}
-            initial='hidden'
-            animate={contactFormInView ? 'visible' : 'hidden'}
-            variants={animationVariants}
-          >
-            <div className='flex items-center justify-center rounded-lg p-4 md:ml-12 lg:h-[700px] lg:border lg:border-black'>
-              <div className='px-2 md:px-1 xl:px-12'>
-                <p className='text-2xl font-bold text-gray-900 md:text-4xl'>
-                  Contact Us
-                </p>
-                <p className='mt-4 text-lg text-primary'>
-                  Our friendly team would love to hear from you.
-                </p>
-                <form
-                  onSubmit={handleSubmit(onSubmit)}
-                  className='mt-8 space-y-4'
-                >
-                  <div className='grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-1'>
-                    <div className='grid w-full items-center gap-1.5'>
-                      <label
-                        className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                        htmlFor='your_name'
-                      >
-                        Your Name <span className='text-red-500'>*</span>
-                      </label>
-                      <input
-                        className='flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
-                        type='text'
-                        id='your_name'
-                        placeholder='Your Name'
-                        required
-                        {...register('name', { required: true })}
-                      />
-                    </div>
-                  </div>
+          <div className='flex items-center justify-center rounded-lg p-4 md:ml-12 lg:h-[700px] lg:border lg:border-black'>
+            <div className='px-2 md:px-1 xl:px-12'>
+              <p className='text-2xl font-bold text-gray-900 md:text-4xl'>
+                Contact Us
+              </p>
+              <p className='mt-4 text-lg text-primary'>
+                Our friendly team would love to hear from you.
+              </p>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className='mt-8 space-y-4'
+              >
+                <div className='grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-1'>
                   <div className='grid w-full items-center gap-1.5'>
                     <label
                       className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                      htmlFor='email'
+                      htmlFor='your_name'
                     >
-                      Email <span className='text-red-500'>*</span>
+                      Your Name <span className='text-red-500'>*</span>
                     </label>
                     <input
                       className='flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
                       type='text'
-                      id='email'
-                      placeholder='Email'
+                      id='your_name'
+                      placeholder='Your Name'
                       required
-                      {...register('email', { required: true })}
+                      {...register('name', { required: true })}
                     />
                   </div>
-                  <div className='grid w-full  items-center gap-1.5'>
-                    <label
-                      className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                      htmlFor='phone_number'
-                    >
-                      Phone number <span className='text-red-500'>*</span>
-                    </label>
-                    <input
-                      className='flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
-                      inputMode='numeric'
-                      id='phone_number'
-                      placeholder='Phone number'
-                      onKeyPress={(e) => {
-                        const pattern = /[0-9]/;
-                        if (!pattern.test(e.key)) {
-                          e.preventDefault();
-                        }
-                      }}
-                      required
-                      {...register('number', { required: true })}
-                    />
-                  </div>
-                  <div className='grid w-full items-center gap-1.5'>
-                    <label
-                      className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                      htmlFor='subject'
-                    >
-                      Subject <span className='text-red-500'>*</span>
-                    </label>
-                    <input
-                      className='flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
-                      type='text'
-                      id='subject'
-                      placeholder='Subject'
-                      required
-                      {...register('subject', { required: true })}
-                    />
-                  </div>
-                  <div className='grid w-full  items-center gap-1.5'>
-                    <label
-                      className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                      htmlFor='message'
-                    >
-                      Message <span className='text-red-500'>*</span>
-                    </label>
-                    <textarea
-                      className='flex h-[150px] w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
-                      id='message'
-                      placeholder='Leave us a message'
-                      cols={3}
-                      required
-                      {...register('message', { required: true })}
-                    />
-                  </div>
-                  <button
-                    type='submit'
-                    className='text-md w-full rounded-md bg-primary px-3 py-2 font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
+                </div>
+                <div className='grid w-full items-center gap-1.5'>
+                  <label
+                    className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                    htmlFor='email'
                   >
-                    Submit
-                  </button>
-                </form>
-              </div>
+                    Email <span className='text-red-500'>*</span>
+                  </label>
+                  <input
+                    className='flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
+                    type='text'
+                    id='email'
+                    placeholder='Email'
+                    required
+                    {...register('email', { required: true })}
+                  />
+                </div>
+                <div className='grid w-full  items-center gap-1.5'>
+                  <label
+                    className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                    htmlFor='phone_number'
+                  >
+                    Phone number <span className='text-red-500'>*</span>
+                  </label>
+                  <input
+                    className='flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
+                    inputMode='numeric'
+                    id='phone_number'
+                    placeholder='Phone number'
+                    onKeyPress={(e) => {
+                      const pattern = /[0-9]/;
+                      if (!pattern.test(e.key)) {
+                        e.preventDefault();
+                      }
+                    }}
+                    required
+                    {...register('number', { required: true })}
+                  />
+                </div>
+                <div className='grid w-full  items-center gap-1.5'>
+                  <label
+                    className='text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                    htmlFor='message'
+                  >
+                    Message <span className='text-red-500'>*</span>
+                  </label>
+                  <textarea
+                    className='flex h-[150px] w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900'
+                    id='message'
+                    placeholder='Leave us a message'
+                    cols={3}
+                    required
+                    {...register('message', { required: true })}
+                  />
+                </div>
+                <button
+                  type='submit'
+                  className='text-md w-full rounded-md bg-primary px-3 py-2 font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
+                >
+                  Submit
+                </button>
+              </form>
             </div>
-          </motion.div>
+          </div>
         </div>
       </div>
       <hr className='mt-6' />
