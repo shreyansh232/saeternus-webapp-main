4ed3dd3769a5a8a64284fbd9bd3bd489a7698e0e
Chhavi Verma <97514667+Violetcv@users.noreply.github.com>
2024-08-17 19:14:26 +0530

    Update course.ts

--- src/api/course/course.ts
+++ src/api/course/course.ts
@@ -378,7 +378,7 @@ const detailedCourses: CourseDetails[] = [
         description: 'Multiple Case Studies; Hands-on Practice.',
       },
     ],
-    startDate: '2024-08-15',
+    startDate: '2024-08-29',
     whatYouWillLearn: [
       'Descriptive Statistics - It includes methodologies such as cross-tabulation, frequencies, and descriptive ratio statistics.',
       'Bivariate Statistics - It includes methodologies such as means, nonparametric tests, correlation, and Analysis of Variance (ANOVA).',
